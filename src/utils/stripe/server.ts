6dd2df04d36143c491d69da1722f7152f2058ed9
J-u-s-t-J-o-s-h <jboynton44@gmail.com>
2026-03-21 02:04:20 -0400

    Fix Stripe initialization error on Vercel build

--- src/utils/stripe/server.ts
+++ src/utils/stripe/server.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ?? '',
+  process.env.STRIPE_SECRET_KEY || 'sk_test_mock_key',
   {
     // @ts-ignore: Bypass strict literal union check on apiVersion 
     apiVersion: '2023-10-16',
